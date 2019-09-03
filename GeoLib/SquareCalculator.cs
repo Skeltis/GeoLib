a9572422f34cb60e1b4eb2dde33d4d311c96c24a
Skeltis <45638983+Skeltis@users.noreply.github.com>
2019-09-03 18:37:51 +0300

    SetFigure implemented

--- GeoLib/SquareCalculator.cs
+++ GeoLib/SquareCalculator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Collections;
+using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using GeoLib.Figures;
 
@@ -20,13 +21,39 @@ namespace GeoLib
         public static double CountTriangleSquare(double sideA, double sideB, double sideC) => 
             (new Triangle(sideA, sideB, sideC)).CountSquare();
 
+        public static void SetFigure<T>(int sideCount) where T : Figure
+        {
+            EnsureConstructorWithSideParamsExist(sideCount);
+            _sideBasedCatalog[sideCount] = typeof(T);
+
+            void EnsureConstructorWithSideParamsExist(int sides)
+            {
+                var constructorsWithDoubles =
+                    (from g
+                     in typeof(T).GetConstructors()
+                     where g.GetParameters().All(par => par.GetType().Equals(typeof(double))) && g.GetParameters().Count() == sides
+                     orderby g.GetParameters().Count() ascending
+                     select g);
+                if (constructorsWithDoubles.Count() < 1)
+                    throw new ArgumentException("The figure must have a constructor with " +
+                        "the number of parameters of the double type corresponding " +
+                        "to the number of sides of the figure");
+            }
+        }
+
         public static double CountSquare(params double[] sideValue)
         {
-            Contract.Requires(_sideBasedCatalog.ContainsKey(sideValue.Length));
+            EnsureFigureStored();
 
             Type figureType = (Type)_sideBasedCatalog[sideValue.Length];
             Figure figure = (Figure)Activator.CreateInstance(figureType, (from g in sideValue select (g as object)).ToArray());
             return figure.CountSquare();
+
+            void EnsureFigureStored()
+            {
+                if (!_sideBasedCatalog.ContainsKey(sideValue.Length))
+                    throw new KeyNotFoundException($"Required figure  with {sideValue.Length} sides doesn't exist");
+            }
         }
     }
 }
